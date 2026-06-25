2154809d4eac243433fd47fa10d09e53d967dd81
GTR-spec <agungrizki990@gmail.com>
2026-06-26 02:41:00 +0700

    Change database connection settings in koneksi.php
    Updated database connection parameters for production environment.

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,17 @@
 <?php
+$server   = "sql213.byetcluster.com";
+$username = "b7_42270965";
+$password = "ballistixtracer";
+$database = "b7_42270965_inventaris_kantor";
+
+$koneksi = mysqli_connect($server, $username, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($koneksi, "utf8mb4");
+?><?php
 $server   = "localhost";
 $username = "root";
 $password = "";
