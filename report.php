ca969477b45f8eb18d93a3aacdf1ff36af340dbd
GTR-spec <agungrizki990@gmail.com>
2026-06-26 09:51:35 +0700

    Enhance PDF report layout and condition checks
    Updated PDF report generation to improve formatting and condition handling.

--- report.php
+++ report.php
@@ -2,27 +2,30 @@
 require_once('vendor/autoload.php');
 include 'koneksi.php';
 
+date_default_timezone_set('Asia/Jakarta');
+
 $pdf = new TCPDF('L', 'mm', 'A4', true, 'UTF-8', false);
 $pdf->setCreator(PDF_CREATOR);
 $pdf->setTitle("Laporan Data Inventaris");
 
-$pdf->setHeaderData('', 0, 'LAPORAN DATA INVENTARIS', "Surface Head Office\nDicetak pada: " . date('d-m-Y'));
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+$pdf->setFont('helvetica', '', 10);
 
-$pdf->setMargins(15, 25, 15);
+$pdf->setHeaderData('', 0, 'LAPORAN DATA INVENTARIS', "Surface Head Office\nDicetak pada: " . date('d-m-Y'));
+$pdf->setHeaderFont(Array('helvetica', '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array('helvetica', '', PDF_FONT_SIZE_DATA));
+
+$pdf->setMargins(15, 30, 15);
 $pdf->setHeaderMargin(10);
 $pdf->setFooterMargin(10);
 
 $pdf->setAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
 $pdf->AddPage();
 
 $query = "SELECT * FROM tb_inventori";
 $result = mysqli_query($koneksi, $query);
 
-$html = '<h2 style="text-align: center; color: #333;">DAFTAR ASET INVENTARIS</h2>';
-$html .= '<table border="1" cellpadding="6" cellspacing="0" style="width: 100%; border-collapse: collapse; font-size: 10pt;">';
+$html = '<h2 style="text-align: center; color: #333; font-family: helvetica;">DAFTAR ASET INVENTARIS</h2>';
+$html .= '<table border="1" cellpadding="6" cellspacing="0" style="width: 100%; border-collapse: collapse; font-family: helvetica; font-size: 9pt;">';
 $html .= '<thead style="background-color: #f2f2f2; font-weight: bold; text-align: center;">
             <tr>
                 <th style="width: 4%;">No</th>
@@ -42,7 +45,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     $no_inv   = htmlspecialchars($row['nomor_inventaris']);
     $nama     = htmlspecialchars($row['nama_barang']);
     $kondisi  = htmlspecialchars($row['kondisi_barang']);
-    $tgl_beli = date('d-m-Y', strtotime($row['tgl_pembelian']));
+    $tgl_beli = ($row['tgl_pembelian'] && $row['tgl_pembelian'] !== '0000-00-00') ? date('d-m-Y', strtotime($row['tgl_pembelian'])) : '-';
     $divisi   = htmlspecialchars($row['divisi']);
     $keterangan   = htmlspecialchars($row['keterangan']);
     
@@ -51,15 +54,15 @@ while ($row = mysqli_fetch_assoc($result)) {
     
     if (!empty($row['gambar']) && file_exists($gambar_file)) {
         $abs_path = realpath($gambar_file);
-        $img_html = '<img src="' . $abs_path . '" style="width: 70px; height: auto;">';
+        $img_html = '<img src="' . $abs_path . '" width="60" height="45" align="middle" />';
     }
 
-    $warna_kondisi = ($kondisi === 'Normal') ? 'color: green;' : 'color: red;';
+    $warna_kondisi = (strcasecmp($kondisi, 'Normal') === 0) ? 'color: #008000;' : 'color: #FF0000;';
 
     $html .= '<tr>
                 <td style="text-align: center; width: 4%;">' . $no++ . '</td>
                 <td style="width: 15%;">' . $no_inv . '</td>
-                <td style="text-align: center; width: 15%;">' . $img_html . '</td>
+                <td style="text-align: center; width: 15%; vertical-align: middle;">' . $img_html . '</td>
                 <td style="width: 18%;">' . $nama . '</td>
                 <td style="text-align: center; ' . $warna_kondisi . ' width: 12%;"><strong>' . $kondisi . '</strong></td>
                 <td style="text-align: center; width: 12%;">' . $tgl_beli . '</td>
@@ -70,7 +73,8 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 $html .= '</tbody></table>';
 
-$pdf->writeHTML($html, true, false, true, false, '');
+if (ob_get_contents()) ob_end_clean();
 
+$pdf->writeHTML($html, true, false, true, false, '');
 $pdf->Output('laporan_inventaris_' . date('Ymd') . '.pdf', 'I');
 ?>
